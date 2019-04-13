acf5b8956c836a8b6b949d7e036df78ac8867bab
Sabique <sabiquekhan@gmail.com>
2019-04-14 01:52:38 +0530

    Setup the app by creting components, routing and navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQh1oyXK5F3biVunuvPL_e_s1uShI3xa0",
+    authDomain: "ng-shop-2a084.firebaseapp.com",
+    databaseURL: "https://ng-shop-2a084.firebaseio.com",
+    projectId: "ng-shop-2a084",
+    storageBucket: "ng-shop-2a084.appspot.com",
+    messagingSenderId: "926432546150"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCQh1oyXK5F3biVunuvPL_e_s1uShI3xa0",
+    authDomain: "ng-shop-2a084.firebaseapp.com",
+    databaseURL: "https://ng-shop-2a084.firebaseio.com",
+    projectId: "ng-shop-2a084",
+    storageBucket: "ng-shop-2a084.appspot.com",
+    messagingSenderId: "926432546150"
+  }
 };
 
 /*
