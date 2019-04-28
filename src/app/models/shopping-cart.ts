562a3d9fe0983efd50d835a790ba82cb0ef68f6c
Sabique <sabiquekhan@gmail.com>
2019-04-29 00:52:08 +0530

    Update the Shopping Cart component to list products

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -3,6 +3,10 @@ import { ShoppingCartItem } from './shopping-cart-item';
 export class ShoppingCart {
     constructor(public items: ShoppingCartItem[]){}
 
+    get productKeys() {
+        return Object.keys(this.items);
+    }
+
     get totalItemsCount() {
         let count = 0;
         for (let productKey in this.items) {
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './../services/shopping-cart.service';
+import { ShoppingCart } from './../models/shopping-cart';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
   }
 
 }
