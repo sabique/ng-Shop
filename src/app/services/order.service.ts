802767cf82ab3bd9864ec1e6540e02ece5808ec8
Sabique <sabiquekhan@gmail.com>
2019-05-05 03:51:31 +0530

    Clear the cart on successful order process

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,20 @@
+import { ShoppingCartService } from './shopping-cart.service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
-import { FirebaseDatabase } from 'angularfire2';
+import { Order } from '../models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(
+    private db: AngularFireDatabase,
+    private shoppingCartService: ShoppingCartService) { }
 
-  placeOrder(order) {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order: Order) {
+    const result = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return result;
   }
 }
