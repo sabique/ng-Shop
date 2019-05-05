da98ca40a2662f7a0ffa877b37119b7a44a9b677
Sabique <sabiquekhan@gmail.com>
2019-05-05 16:07:08 +0530

    Modified the layout of the navigation bar and login page

---
+++ src/app/shared/models/shopping-cart.ts
@@ -0,0 +1,39 @@
+import { Product } from './product';
+import { ShoppingCartItem } from './shopping-cart-item';
+
+export class ShoppingCart {
+    itemsMap: ShoppingCartItem[] = [];
+    constructor(private items: { [productId: string]: ShoppingCartItem }) {
+        this.items = items || {};
+        // tslint:disable-next-line: forin
+        for(let productId in items) {
+            let item = items[productId];
+
+            this.itemsMap.push(new ShoppingCartItem({ ...item, $key: productId }));
+        }
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        // tslint:disable-next-line: forin
+        for(let productId in this.itemsMap) {
+            sum += this.itemsMap[productId].totalPrice;
+        }
+        return sum;
+    }
+
+    get totalItemsCount() {
+        let count = 0;
+        // tslint:disable-next-line: forin
+        for (let productKey in this.items) {
+            count += this.items[productKey].quantity;
+        }
+        return count;
+    }
+
+    getQuantity(product: Product) {
+        // tslint:disable-next-line: no-string-literal
+        const item = this.items[product['$key']];
+        return item ? item.quantity : 0;
+    }
+}
